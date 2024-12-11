7991d16afe7d3725d56bc75c74a833a6c1ff4756
ahmarshahid <ahmarjan456@gmail.com>
2024-12-11 22:31:15 +0500

    Signin form implemented

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -5,13 +5,9 @@ import Image from 'next/image'
 import { useState } from 'react'
 
 import { z } from "zod"
- 
+ import { authformSchema } from '@/lib/utils';
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    
-  }),
-})
+
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -26,31 +22,26 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import CustomInput from './CustomInput';
 
 const AuthForm = ({type}: {type:string}) => {
     const [user, setUser] = useState(null)
 
      // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof authformSchema>>({
+    resolver: zodResolver(authformSchema),
     defaultValues: {
-      username: "",
+      email: "",
+      password: " "
     },
   })
  
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  // submit handler.
+  function onSubmit(values: z.infer<typeof authformSchema>) {
+
     console.log(values)
   }
 
-
-
-
-
-
-    
   return (
     <section className='"auth-form'>
        <header className='flex flex-col gap-5 md:gap-8'>
@@ -88,10 +79,26 @@ const AuthForm = ({type}: {type:string}) => {
         </div>
        ):(
         <>
-        FORM
+            <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+           
+          <CustomInput control = {form.control}
+          name = "Username" Label = "Username"
+          placeholder = "Enter your username"
+          type = "email" 
+          />
+            
+            <CustomInput control = {form.control}
+            name = "Password" Label = "Password"
+            placeholder = "Enter your password" 
+            type = "password"/>
+
+
+            <Button type="submit">Submit</Button>
+        </form>
+        </Form>
         </>
-       )
-       }
+       )}
     </section>
   )
 }
---
+++ components/CustomInput.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { FormField, FormLabel, FormMessage, FormControl } from '@/components/ui/form'
+import {Input} from './ui/input'
+import { z } from 'zod'
+import {Control} from 'react-hook-form'
+import {authformSchema} from '@/lib/utils'
+
+interface CustomInput {
+    control: Control<z.infer<typeof authformSchema>>,
+    name: string,
+    Label: string,
+    placeholder: string,
+    type: string
+}
+
+
+
+
+const CustomInput = ({control, name, Label, placeholder, type}: CustomInput) => {
+  return (
+    <FormField
+            control={control}
+            name= {name}
+            render={({ field }) => (
+                <div className='form-item'>
+                   <FormLabel className='form-label'>
+                    {Label}
+                    </FormLabel> 
+                    <div className= "flex w-full flex-col">
+                      <FormControl>
+                        <Input
+                         placeholder= {placeholder}
+                         className = "input-class"
+                         type= {type}
+                         {...field}
+                        />
+                      </FormControl>
+                      <FormMessage
+                      className='form-message mt-2' />
+                    </div>
+                </div>
+            )}
+            />
+  )
+}
+
+export default CustomInput
