a7c684e136777a2607640f214d4f1c700a4d309b
Haya Fatima <hfatima.1049@gmail.com>
2025-01-02 03:12:53 +0500

    Update Card Styling

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -9,36 +9,36 @@ const Cards = ({
     showBalance = true
 }: CreditCardProps) => {
   return (
-    <div className="flex flex-col">
-        <Link href="/" className="bank-card">
-            <div className="bank-card_content">
+    <div className="flex flex-col shadow-md rounded-lg overflow-hidden">
+        <Link href="/" className="bank-card bg-gradient-to-r from-blue-500 to-indigo-600 p-4 relative">
+            <div className="bank-card_content flex flex-col justify-between h-full">
                 <div>
                     <h1 className="text-16 font-semibold text-white">
                         {userName}
                     </h1>
                     {showBalance && (
-                        <p className="font-ibm-plex-serif font-black text-white">
+                        <p className="font-ibm-plex-serif font-black text-white text-xl">
                             {formatAmount(account.currentBalance)}
                         </p>
                     )}
                 </div>
-                <article className="flex flex-col gap-2">
+                <article className="flex flex-col gap-2 mt-4">
                     <div className="flex justify-between">
                         <h1 className="text-12 font-semibold text-white">
                             {userName}
                         </h1>
                         <h2 className="text-12 font-semibold text-white">
-                            ●●/●●
+                            12/25
                         </h2> 
                     </div>
                     <p className="text-12 font-semibold tracking-[1.1px] text-white">
                         ●●●● ●●●● ●●●● <span className="text-16">
-                            0597
+                            {account.cardLastFour}
                         </span>
                     </p>
                 </article>
             </div>
-            <div className="bank-card_icon">
+            <div className="bank-card_icon flex items-center gap-3 absolute bottom-4 right-4">
                 <Image
                     src="/icons/paypass.svg"
                     width={20}
@@ -50,7 +50,6 @@ const Cards = ({
                     width={49}
                     height={39}
                     alt="mastercard"
-                    className="ml-5"
                 />
             </div>
             <Image
@@ -58,7 +57,7 @@ const Cards = ({
                 width={316}
                 height={190}
                 alt="lines"
-                className="absolute top-0 left-0"
+                className="absolute top-0 left-0 opacity-30"
             />
         </Link>
     </div>
